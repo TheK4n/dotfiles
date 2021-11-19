e3f9a64b1d0594afeaa1cf20c726df306045a0ff
thek4n <djvlad967891@gmail.com>
2021-11-19 14:37:08 +0300

    luks

--- functions/luks.sh
+++ functions/luks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root"
+    echo "luks: Please run as root" >&2
     exit 1
 fi
 
@@ -10,14 +10,18 @@ while getopts ":c :o" opt; do
 
     case $opt in
       c)
-        sudo umount /mnt/Files && sudo cryptsetup luksClose Files
+        sudo umount /mnt/Files &>/dev/null && sudo cryptsetup luksClose Files || echo "luks: Not mounted" >&2
+        exit 0
         ;;
       o)
         sudo cryptsetup luksOpen /dev/sdb5 Files && sudo mount /dev/mapper/Files /mnt/Files
+        exit 0
         ;;
       \?)
-        echo "Invalid option: -$OPTARG" >&2
+        echo "luks: Invalid option: -$OPTARG" >&2 && exit 2
         ;;
     esac
 
 done
+
+echo "luks: No option, -o to open, -c to close" >&2 && exit 3
