24b2a44de15d5715d1b8cf5bf8ea87adcf6d8779
TheK4n <djvlad967891@gmail.com>
2023-10-23 01:15:05 +0300

    nvim(feat): linter

--- home/user/.config/nvim/lua/base/plugins/mason.lua
+++ home/user/.config/nvim/lua/base/plugins/mason.lua
@@ -39,7 +39,9 @@ return {
         },
         config = function()
             require("mason").setup()
-            local mason_lspconfig = require("mason-lspconfig") mason_lspconfig.setup()
+            local mason_lspconfig = require("mason-lspconfig")
+
+            mason_lspconfig.setup()
 
             mason_lspconfig.setup_handlers({
                 function (server_name)
