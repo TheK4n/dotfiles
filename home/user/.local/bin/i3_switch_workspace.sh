d3eb1b29c2242e65f020f6e376f6e813e1d339e2
Thek4n <djvlad967891@gmail.com>
2023-11-20 10:13:43 +0300

    feat(rofi): switch i3 workspaces

---
+++ home/user/.local/bin/i3_switch_workspace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function gen_workspaces()
+{
+    i3-msg -t get_workspaces | tr ',' '\n' | grep "name" | sed 's/"name":"\(.*\)"/\1/g'
+}
+
+
+WORKSPACE=$(gen_workspaces | rofi -dmenu -i -p "Select workspace")
+
+if [ -n "${WORKSPACE}" ]
+
+then
+    i3-msg workspace "${WORKSPACE}"
+fi
