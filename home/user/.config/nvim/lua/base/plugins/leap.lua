cd8b9cbd63e0bb6534ce5dc165bfb80695c07083
thek4n <thek4n@yandex.ru>
2023-12-21 09:15:37 +0300

    leap disable autojump

--- home/user/.config/nvim/lua/base/plugins/leap.lua
+++ home/user/.config/nvim/lua/base/plugins/leap.lua
@@ -1,7 +1,10 @@
 return {
     'ggandor/leap.nvim',
+    config = function()
+        require('leap').opts.safe_labels = {}
+    end,
     keys = {
         { 'J', '<Plug>(leap-forward-to)' },
         { 'K', '<Plug>(leap-backward-to)' },
-    }
+    },
 }
