660c9b8c406c40cc77cd1820023cf3ee82b7b720
thek4n <thek4n@yandex.ru>
2024-01-22 19:30:30 +0300

    nvim textobjs

---
+++ home/user/.config/nvim/lua/base/plugins/vim_textobj.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'kana/vim-textobj-lastpat', -- text-object i/, operate under finding
+        dependencies = { 'kana/vim-textobj-user' }
+    },
+    {
+        'sgur/vim-textobj-parameter',
+        dependencies = { 'kana/vim-textobj-user' }
+    }
+}
--- home/user/.config/nvim/lua/base/plugins/vim_textobj_lastpat.lua
+++
@@ -1,4 +0,0 @@
-return {
-    'kana/vim-textobj-lastpat', -- text-object i/, operate under finding
-    dependencies = { 'kana/vim-textobj-user' }
-}
