bcbdd2b4755a57e674d00ea7c8cb8143895de63e
thek4n <thek4n@yandex.ru>
2024-02-06 13:37:29 +0300

    gitignore bak files with postfix ~

--- home/user/.config/nvim/lua/base/options.lua
+++ home/user/.config/nvim/lua/base/options.lua
@@ -12,7 +12,7 @@ opt.relativenumber = true
 opt.clipboard:append { 'unnamed' }
 opt.clipboard:append { 'unnamedplus' }
 
-opt.shortmess:append{a = true, s = true, I = true, c = true, S = true }  -- disable startup message
+opt.shortmess:append{a = true, s = true, I = true, c = true, S = true}  -- disable startup message
 
 opt.cmdheight = 1
 vim.wo.signcolumn = "yes:1"
