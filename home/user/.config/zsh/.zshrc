5f7473dd658bad4196992ba74b801411d5931558
thek4n <thek4n@yandex.ru>
2024-03-27 21:54:25 +0300

    tmux fix session manager

--- home/user/.config/zsh/.zshrc
+++ home/user/.config/zsh/.zshrc
@@ -1,6 +1,10 @@
 # .zshrc by TheK4n
 # https://github.com/TheK4n/dotfiles
 
+if command -v tmux &>/dev/null && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+    exec tmux
+fi
+
 if [ -f "$ZDOTDIR/sourcer" ]; then
     source "$ZDOTDIR/sourcer"
 fi
---
+++ home/user/.tmux/01.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+
+SESSION=s01
+MAINW=1
+
+WORKING_PROJECT="$HOME/code/shell/note"
+EDITOR="vi"
+
+if ! tmux has-session -t "$SESSION"; then
+    tmux new-session -s "$SESSION" -d -n "$MAINW" -c "$WORKING_PROJECT"
+    tmux send-keys -t "$SESSION:$MAINW" "$EDITOR" Enter
+    tmux send-keys -t "$SESSION:$MAINW" , 1
+    tmux splitw -t "$SESSION:$MAINW" -d -h -l 40% -c '#{pane_current_path}'
+fi
+
+tmux switch-client -t "$SESSION"
