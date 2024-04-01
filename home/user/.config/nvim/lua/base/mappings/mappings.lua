165bd860256af091a4b69d78bcb600d2905fd97b
thek4n <thek4n@yandex.ru>
2024-04-01 11:21:09 +0300

    nvim add encodings

--- home/user/.config/nvim/lua/base/mappings/mappings.lua
+++ home/user/.config/nvim/lua/base/mappings/mappings.lua
@@ -141,3 +141,16 @@ map("n", "<Leader>pl", "<cmd>Lazy<CR>",
 
 map("n", "<Leader>pm", "<cmd>Mason<CR>",
     {desc = "LSP Manager"})
+
+
+vim.cmd(
+[[
+menu Encoding.koi8-r :e ++enc=koi8-r ++ff=unix<CR>
+menu Encoding.windows-1251 :e ++enc=cp1251 ++ff=dos<CR>
+menu Encoding.cp866 :e ++enc=cp866 ++ff=dos<CR>
+menu Encoding.utf-8 :e ++enc=utf8<CR>
+menu Encoding.koi8-u :e ++enc=koi8-u ++ff=unix<CR>
+
+map <F8> :emenu Encoding.
+]]
+)
