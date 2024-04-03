7bb9a3ca97b9dfcd3815aa83120e079aed6c7c6b
thek4n <thek4n@yandex.ru>
2024-04-03 11:24:39 +0300

    tmux session

--- home/user/.tmux/01.sh
+++ home/user/.tmux/01.sh
@@ -2,16 +2,16 @@
 
 set -x
 
-SESSION=s01
+SESSION=dotfiles
 MAINW=1
 
-WORKING_PROJECT="$HOME/code/shell/note"
-EDITOR="vi"
+WORKING_PROJECT="$HOME/dotfiles"
 
 if ! tmux has-session -t "$SESSION"; then
     tmux new-session -s "$SESSION" -d -n "$MAINW" -c "$WORKING_PROJECT"
-    tmux send-keys -t "$SESSION:$MAINW" "$EDITOR" Enter
-    tmux splitw -t "$SESSION:$MAINW" -d -h -l 40% -c '#{pane_current_path}'
+    tmux send-keys -t "$SESSION:$MAINW" '$EDITOR' Enter
+    tmux send-keys -t "$SESSION:$MAINW" , f f
+    tmux splitw -t "$SESSION:$MAINW" -d -h -l 35% -c '#{pane_current_path}'
 fi
 
 tmux switch-client -t "$SESSION"
