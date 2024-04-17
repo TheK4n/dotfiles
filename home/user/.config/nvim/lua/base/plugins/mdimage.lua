d8fef7e944e5b94e6b4c657bb0ba5698d52bd1b7
thek4n <thek4n@yandex.ru>
2024-04-17 14:19:21 +0300

    fix(mdimage-plugin): change target dir

--- home/user/.config/nvim/lua/base/plugins/mdimage.lua
+++ home/user/.config/nvim/lua/base/plugins/mdimage.lua
@@ -2,7 +2,7 @@ return {
     "thek4n/mdimage.nvim",
     config = function()
         require("mdimage").setup({
-            target_path = vim.env.HOME .. "/.notes/.img",
+            target_path = "./.img",
         })
     end
 }
