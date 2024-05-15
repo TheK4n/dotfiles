abe09fec67cb683d67a52267e8703a7c0b080837
thek4n <thek4n@yandex.ru>
2024-05-15 08:53:32 +0300

    add(firefox): config

---
+++ home/user/.mozilla/firefox/user.js
@@ -0,0 +1,4 @@
+user_pref("browser.fullscreen.autohide", false);
+user_pref("suggest.calculator", true);
+lockPref("extensions.screenshots.disabled", true);
+defaultPref("browser.tabs.closeWindowWithLastTab", false);
