325c2a9f2597dbbb31116738c685023ce1b6210d
thek4n <thek4n@yandex.ru>
2024-05-23 19:39:21 +0300

    some ref

--- home/user/.tmux/01.sh
+++ home/user/.tmux/01.sh
@@ -9,9 +9,10 @@ WORKING_PROJECT="$HOME/dotfiles"
 
 if ! tmux has-session -t "$SESSION"; then
     tmux new-session -s "$SESSION" -d -n "$MAINW" -c "$WORKING_PROJECT"
-    tmux send-keys -t "$SESSION:$MAINW" '$EDITOR' Enter
-    tmux send-keys -t "$SESSION:$MAINW" , f f
-    tmux splitw -t "$SESSION:$MAINW" -d -b -h -l $_tmux_new_pane_size -c '#{pane_current_path}'
+    tmux splitw -t "$SESSION:$MAINW" -d -b -h -l "$_tmux_new_pane_size" -c '#{pane_current_path}'
+    tmux send-keys -t "$SESSION:$MAINW.2" '$EDITOR' Enter
+    tmux send-keys -t "$SESSION:$MAINW.2" , f f
+    tmux send-keys -t "$SESSION:$MAINW.1" "git pull" Enter
 fi
 
 tmux switch-client -t "$SESSION"
--- home/user/.tmux/10.sh
+++ home/user/.tmux/10.sh
@@ -9,3 +9,4 @@ WORKING_PROJECT="$(mktemp -d)"
 
 tmux new-session -s "$SESSION" -d -n "$MAINW" -c "$WORKING_PROJECT"
 tmux switch-client -t "$SESSION"
+tmux splitw -t "$SESSION:$MAINW" -d -b -h -l "$_tmux_new_pane_size" -c '#{pane_current_path}'
