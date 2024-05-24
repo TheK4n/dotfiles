5832c0d70754ad49c4c0d49789672b155930b6be
thek4n <thek4n@yandex.ru>
2024-05-24 16:50:26 +0300

    tmux mappings

--- home/user/.tmux/10.sh
+++ home/user/.tmux/10.sh
@@ -10,3 +10,4 @@ WORKING_PROJECT="$(mktemp -d)"
 tmux new-session -s "$SESSION" -d -n "$MAINW" -c "$WORKING_PROJECT"
 tmux switch-client -t "$SESSION"
 tmux splitw -t "$SESSION:$MAINW" -d -b -h -l "$_tmux_new_pane_size" -c '#{pane_current_path}'
+tmux send-keys -t "$SESSION:$MAINW.1" 'git clone  .' Left Left
