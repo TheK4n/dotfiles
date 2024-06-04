b34161c81130c12ec7a1c2e35be47afaaeb090e5
thek4n <thek4n@yandex.ru>
2024-06-04 09:04:40 +0300

    fix tmux session 10

--- home/user/.tmux/10.sh
+++ home/user/.tmux/10.sh
@@ -9,5 +9,5 @@ WORKING_PROJECT="$(mktemp -d)"
 
 tmux new-session -s "$SESSION" -d -n "$MAINW" -c "$WORKING_PROJECT"
 tmux switch-client -t "$SESSION"
-tmux splitw -t "$SESSION:$MAINW" -d -b -h -l "$_tmux_new_pane_size" -c '#{pane_current_path}'
+tmux splitw -t "$SESSION:$MAINW" -d -b -h -l "$_tmux_other_pane_size" -c '#{pane_current_path}'
 tmux send-keys -t "$SESSION:$MAINW.1" 'git clone  .' Left Left
