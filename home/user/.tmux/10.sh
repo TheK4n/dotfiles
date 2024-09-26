62dd4101e4fced2988a79141e8e2d57dcf64beec
thek4n <thek4n@yandex.ru>
2024-09-26 15:38:16 +0300

    tmux sesions

--- home/user/.tmux/10.sh
+++ home/user/.tmux/10.sh
@@ -7,7 +7,10 @@ MAINW=1
 
 WORKING_PROJECT="$(mktemp -d)"
 
-tmux new-session -s "$SESSION" -d -n "$MAINW" -c "$WORKING_PROJECT"
-tmux switch-client -t "$SESSION"
+tmux new-session -s "$SESSION" -d -n "$MAINW"
 tmux splitw -t "$SESSION:$MAINW" -d -b -h -l "$_tmux_other_pane_size" -c '#{pane_current_path}'
-tmux send-keys -t "$SESSION:$MAINW.1" 'git clone  .' Left Left
+
+tmux new-window  -d -n "2" -t "$SESSION:2" -c "$WORKING_PROJECT"
+tmux splitw -t "$SESSION:2" -d -b -h -l "$_tmux_other_pane_size" -c "$WORKING_PROJECT"
+tmux send-keys -t "$SESSION:2.1" 'git clone  .' Left Left
+tmux switch-client -t "$SESSION"
