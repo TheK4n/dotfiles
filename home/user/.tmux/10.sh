2404c83783a7d06c18efc2c78787178ff3a5261a
thek4n <thek4n@yandex.ru>
2024-09-27 09:30:24 +0300

    fix tmux session 10

--- home/user/.tmux/10.sh
+++ home/user/.tmux/10.sh
@@ -7,7 +7,7 @@ MAINW=1
 
 WORKING_PROJECT="$(mktemp -d)"
 
-tmux new-session -s "$SESSION" -d -n "$MAINW"
+tmux new-session -s "$SESSION" -d -n "$MAINW" -c ~
 tmux splitw -t "$SESSION:$MAINW" -d -b -h -l "$_tmux_other_pane_size" -c '#{pane_current_path}'
 
 tmux new-window  -d -n "2" -t "$SESSION:2" -c "$WORKING_PROJECT"
