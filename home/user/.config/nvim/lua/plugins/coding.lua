8a718dfb3cf21e8dec776fc612d58616f1cceb23
thek4n <thek4n@yandex.ru>
2024-10-16 14:14:32 +0300

    code style

--- home/user/.config/nvim/lua/plugins/coding.lua
+++ home/user/.config/nvim/lua/plugins/coding.lua
@@ -167,29 +167,29 @@ return {
         end
     },
     {
-    "aznhe21/actions-preview.nvim",
-    dependencies = {
-        'nvim-telescope/telescope.nvim',
-        'MunifTanjim/nui.nvim'
-    },
-    config = function()
-        require("actions-preview").setup {
-            telescope = {
-                sorting_strategy = "ascending",
-                layout_strategy = "vertical",
-                layout_config = {
-                    width = 0.8,
-                    height = 0.9,
-                    prompt_position = "top",
-                    preview_cutoff = 20,
-                    preview_height = function(_, _, max_lines)
-                        return max_lines - 15
-                    end,
+        "aznhe21/actions-preview.nvim",
+        dependencies = {
+            'nvim-telescope/telescope.nvim',
+            'MunifTanjim/nui.nvim'
+        },
+        config = function()
+            require("actions-preview").setup {
+                telescope = {
+                    sorting_strategy = "ascending",
+                    layout_strategy = "vertical",
+                    layout_config = {
+                        width = 0.8,
+                        height = 0.9,
+                        prompt_position = "top",
+                        preview_cutoff = 20,
+                        preview_height = function(_, _, max_lines)
+                            return max_lines - 15
+                        end,
+                    },
                 },
-            },
-        }
-        vim.keymap.set("n", "<space>c", "", {desc = "Code action"})
-        vim.keymap.set("n", "<space>ca", require("actions-preview").code_actions, {desc = "Code action"})
-    end,
+            }
+            vim.keymap.set("n", "<space>c", "", {desc = "Code action"})
+            vim.keymap.set("n", "<space>ca", require("actions-preview").code_actions, {desc = "Code action"})
+        end,
     },
 }
