d6db0e4b482a42d257e90b9fa7a59df9615874e7
thek4n <thek4n@yandex.ru>
2024-10-21 19:07:18 +0300

    tmux session fzf

--- home/user/.local/bin/tmux_start_session.sh
+++ home/user/.local/bin/tmux_start_session.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
 
-readonly TMUX_SESSIONS="${HOME}/.tmux"
+export FZF_DEFAULT_OPTS="\
+--cycle \
+--pointer='' \
+--marker='+' \
+--color='pointer:green' \
+--bind \
+ctrl-/:toggle-preview,\
+ctrl-p:preview-up,\
+ctrl-n:preview-down,\
+ctrl-u:clear-query,\
+ctrl-q:toggle+down\
+"
+
+declare -r TMUX_SESSIONS="${HOME}/.tmux"
 
 declare sessions
 sessions="$(find "${TMUX_SESSIONS}/" -type f -executable)"
 readonly sessions
 
 declare session_file
-session_file="$(echo "${sessions}" | xargs -n 1 basename | fzf +m)"
+session_file="$(echo "${sessions}" | xargs -n 1 basename | fzf +m --preview-window 70% --preview "cat '${TMUX_SESSIONS}/{1}'")"
 readonly session_file
 
 if [[ -n "${session_file}" ]]; then
