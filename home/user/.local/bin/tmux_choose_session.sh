033425f8690b9872a942d10a67754af49abc5f35
thek4n <thek4n@yandex.ru>
2024-10-21 12:40:22 +0300

    tmux mapping choose session fix

--- home/user/.local/bin/tmux_choose_session.sh
+++ home/user/.local/bin/tmux_choose_session.sh
@@ -17,13 +17,18 @@ tab:replace-query,\
 shift-tab:toggle\
 "
 
+declare current_session
+current_session="$(tmux display-message -p '#S')"
+readonly current_session
+
 declare sessions
-sessions="$(tmux list-session -F '#S')"
+sessions="$(tmux list-session -F '#S#{?session_attached, (attached),}')"
+sessions="$(echo "${sessions}" | sed "s/^${current_session} /${current_session} (current) /")"
 readonly sessions
 
 
 declare session
-session="$(echo "${sessions}" | fzf +m --preview-window 75%,follow --preview 'tmux capture-pane -p -e -t {1}')"
+session="$(echo "${sessions}" | fzf +m --preview-window 75%,follow --preview 'tmux capture-pane -p -e -t {1}' | awk '{printf $1}')"
 readonly session
 
 if [[ -n "${session}" ]]; then
