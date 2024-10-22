ba786afab4156d9c05f6649005267f3b109418c6
thek4n <thek4n@yandex.ru>
2024-10-22 22:30:39 +0300

    tmux start session refactor

--- home/user/.local/bin/tmux_start_session.sh
+++ home/user/.local/bin/tmux_start_session.sh
@@ -16,12 +16,27 @@ ctrl-q:toggle+down\
 
 declare -r TMUX_SESSIONS="${HOME}/.tmux"
 
+
+declare current_session
+current_session="$(tmux display-message -p '#S')"
+readonly current_session
+
+declare existing_sessions
+existing_sessions="$(tmux list-session -F '#S')"
+readonly existing_sessions
+
 declare sessions
-sessions="$(find "${TMUX_SESSIONS}/" -type f -executable)"
+sessions="$(find "${TMUX_SESSIONS}/" -type f -executable -exec basename {} \;)"
+sessions="$(echo "${sessions}" | sed "s/^${current_session}/${current_session} (current)/")"
+
+for _s in $existing_sessions
+do
+    sessions="$(echo "${sessions}" | sed "s/^${_s}/${_s} (created)/")"
+done
 readonly sessions
 
 declare session_file
-session_file="$(echo "${sessions}" | xargs -n 1 basename | fzf +m --preview-window 70% --preview "cat '${TMUX_SESSIONS}/{1}'")"
+session_file="$(echo "${sessions}" | fzf +m --preview-window 70% --preview "cat '${TMUX_SESSIONS}/{1}'" | awk '{printf $1}')"
 readonly session_file
 
 if [[ -n "${session_file}" ]]; then
