737027bf5459dd39c7818c8d3c2ff962d194ba1d
thek4n <thek4n@yandex.ru>
2024-11-01 21:28:14 +0300

    ref tmux scripts

--- home/user/.local/bin/tmux_attach_session.sh
+++ home/user/.local/bin/tmux_attach_session.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env sh
 
 
+readonly tmux_list_sessions_script="$HOME/.local/bin/tmux_list_sessions.sh"
+
 export FZF_DEFAULT_OPTS="\
 --cycle \
 --pointer='' \
 --color='pointer:blue,prompt:blue' \
+--no-multi \
+--preview-window 70%,follow \
+--preview 'tmux capture-pane -p -e -t {1}'
 --bind \
 ctrl-/:toggle-preview,\
 ctrl-p:preview-up,\
 ctrl-n:preview-down,\
-ctrl-x:execute\(tmux\ kill-session\ -t\ {1}\)+reload\(~/.local/bin/tmux_list_sessions.sh\),\
-ctrl-u:clear-query\
+ctrl-u:clear-query,\
+ctrl-x:execute\(tmux\ kill-session\ -t\ {1}\)+reload\(${tmux_list_sessions_script}\),\
+load:reload\(${tmux_list_sessions_script}\;sleep\ 1\)\
 "
 
-session_to_attach="$(~/.local/bin/tmux_list_sessions.sh | fzf +m --preview-window 70%,follow --preview 'tmux capture-pane -p -e -t {1}' | awk '{printf $1}')"
+session_to_attach="$("${tmux_list_sessions_script}" | fzf | awk '{printf $1}')"
 readonly session_to_attach
 
 if [ -n "${session_to_attach}" ]; then
--- home/user/.local/bin/tmux_kill_sessions.sh
+++ home/user/.local/bin/tmux_kill_sessions.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env sh
 
 
+readonly tmux_list_sessions_script="$HOME/.local/bin/tmux_list_sessions.sh"
+
 export FZF_DEFAULT_OPTS="\
 --cycle \
 --pointer='' \
 --marker='+' \
 --color='pointer:red,prompt:red' \
+--multi \
+--preview-window 70%,follow \
+--preview 'tmux capture-pane -p -e -t {1}' \
 --bind \
 ctrl-/:toggle-preview,\
+ctrl-a:toggle-all,\
 ctrl-p:preview-up,\
 ctrl-n:preview-down,\
-ctrl-x:execute\(tmux\ kill-session\ -t\ {1}\)+reload\(~/.local/bin/tmux_list_sessions.sh\),\
 ctrl-u:clear-query,\
-ctrl-o:toggle\
+ctrl-o:toggle,\
+ctrl-x:execute\(tmux\ kill-session\ -t\ {1}\)+reload\(${tmux_list_sessions_script}\)\
 "
 
-sessions_to_kill="$(~/.local/bin/tmux_list_sessions.sh | fzf -m --preview-window 70%,follow --preview 'tmux capture-pane -p -e -t {1}' | awk '{printf $1"\n"}')"
+sessions_to_kill="$(~/.local/bin/tmux_list_sessions.sh | fzf | awk '{printf $1"\n"}')"
 readonly sessions_to_kill
 
 if [ -n "${sessions_to_kill}" ]; then
