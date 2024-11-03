1706e7bda9bd96e0c6caf08c18f3dfe924547ce8
thek4n <thek4n@yandex.ru>
2024-11-03 23:21:52 +0300

    rewrite install script to posix sh compatible

--- TARGETS.sh
+++ TARGETS.sh
@@ -1,21 +1,20 @@
-declare -r -A TARGETS=(
-    ["colors"]=".config/terminal-colors.d"
-    ["tmux"]=".config/tmux .tmux .config/systemd/user/tmux.service .local/bin/tmux_start_session.sh .local/bin/tmux_list_sessions.sh .local/bin/tmux_attach_session.sh .local/bin/tmux_kill_sessions.sh"
-    ["zsh"]=".config/zsh .zshenv .inputrc %colors"
-    ["alacritty"]=".config/alacritty"
-    ["nvim"]=".config/nvim .editorconfig .inputrc .editrc .local/bin/vim_askpass_helper"
-    ["ssh"]=""
-    ["less"]=".lesskey"
-    ["git"]=".config/git"
-    ["ranger"]=".config/ranger"
-    ["gpg"]=""
-    ["i3"]=".xinitrc .xprofile .Xresources .config/i3 .config/i3status .local/bin/i3status_wrapper .config/rofi .config/picom .local/bin/slm .local/bin/slm_rofi.sh .local/bin/power_rofi.sh .local/bin/wifi .local/bin/bluetooth .local/bin/i3_switch_workspace.sh"
-    ["bat"]=".config/bat"
-    ["font"]=""
-    ["termux"]=".termux"
-    ["arch"]=""
-    ["psql"]=".psqlrc"
-    ["docker"]=".docker/cli-plugins"
-    ["ipython"]=".ipython/profile_default/ipython_config.py"
-    ["gdb"]=".config/gdb"
-)
+readonly TARGETS="\
+colors:.config/terminal-colors.d
+tmux:.config/tmux .tmux .config/systemd/user/tmux.service .local/bin/tmux_start_session.sh .local/bin/tmux_list_sessions.sh .local/bin/tmux_attach_session.sh .local/bin/tmux_kill_sessions.sh
+zsh:.config/zsh .zshenv .inputrc %colors
+alacritty:.config/alacritty
+nvim:.config/nvim .editorconfig .inputrc .editrc .local/bin/vim_askpass_helper
+ssh:
+less:.lesskey
+git:.config/git
+ranger:.config/ranger
+gpg:
+i3:.xinitrc .xprofile .Xresources .config/i3 .config/i3status .local/bin/i3status_wrapper .config/rofi .config/picom .local/bin/slm .local/bin/slm_rofi.sh .local/bin/power_rofi.sh .local/bin/wifi .local/bin/bluetooth .local/bin/i3_switch_workspace.sh
+bat:.config/bat
+font:
+termux:.termux
+arch:
+psql:.psqlrc
+docker:.docker/cli-plugins
+ipython:.ipython/profile_default/ipython_config.py
+gdb:.config/gdb"
