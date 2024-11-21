7f217ecbd7760db0bf350fbbdd4d04e306458318
thek4n <thek4n@yandex.ru>
2024-11-21 13:33:55 +0300

    nvim fix go indention

---
+++ home/user/.config/nvim/after/ftplugin/go.lua
@@ -0,0 +1,5 @@
+local opt = vim.opt_local
+
+opt.expandtab = false
+opt.tabstop = 4
+opt.shiftwidth = 4
--- home/user/.config/nvim/after/ftplugin/python.lua
+++ home/user/.config/nvim/after/ftplugin/python.lua
@@ -5,26 +5,3 @@ vim.g.python_indent = {
     closed_paren_align_last_line = false,
     searchpair_timeout = 150,
 }
-
-
-vim.api.nvim_create_autocmd("InsertCharPre", {
-  pattern = { "*.py" },
-  group = vim.api.nvim_create_augroup("py-fstring", { clear = true }),
-  ---@param params NvimAutocmdCallbackParams
-  callback = function(params)
-    if vim.v.char ~= "{" then return end
-
-    local node = vim.treesitter.get_node({})
-
-    if not node then return end
-
-    if node:type() ~= "string" then node = node:parent() end
-
-    if not node or node:type() ~= "string" then return end
-    local row, col, _, _ = vim.treesitter.get_node_range(node)
-    local first_char = vim.api.nvim_buf_get_text(params.buf, row, col, row, col + 1, {})[1]
-    if first_char == "f" then return end
-
-    vim.api.nvim_input("<Esc>m'" .. row + 1 .. "gg" .. col + 1 .. "|if<esc>`'la")
-  end,
-})
