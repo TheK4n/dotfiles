3aa127e7e6c104199cbed6210674ba60f1f496b6
thek4n <thek4n@yandex.ru>
2024-12-19 12:11:40 +0300

    chore: move rarely used zsh functions to files

--- TARGETS.sh
+++ TARGETS.sh
@@ -20,4 +20,5 @@ docker:.docker/cli-plugins
 ipython:.ipython/profile_default/ipython_config.py
 gdb:.config/gdb
 sandbox:
+utils: .local/bin/myip .local/bin/genpass .local/bin/gensalt .local/bin/django-create-project .local/bin/mirror-site .local/bin/split-file .local/bin/extract .local/bin/destroy
 all:%less %t %tmux %zsh %nvim %git"
