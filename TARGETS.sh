e09b56351d44682d42607123c021655653696871
thek4n <thek4n@yandex.ru>
2025-05-15 16:17:30 +0300

    add default xdg apps

--- TARGETS.sh
+++ TARGETS.sh
@@ -11,7 +11,7 @@ ssh:
 git:.config/git
 ranger:.config/ranger
 gpg:
-i3:.xinitrc .xprofile .Xresources .config/i3 .config/i3status .local/bin/i3status_wrapper .config/rofi .config/picom .local/bin/slm .local/bin/slm_rofi.sh .local/bin/power_rofi.sh .local/bin/i3_switch_workspace.sh
+i3:.xinitrc .xprofile .Xresources .config/i3 .config/i3status .local/bin/i3status_wrapper .config/rofi .config/picom .local/bin/slm .local/bin/slm_rofi.sh .local/bin/power_rofi.sh .local/bin/i3_switch_workspace.sh .config/mimeapps.list
 bat:.config/bat
 font:
 termux:.termux
