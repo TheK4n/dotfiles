f8f7831415977f59b5af28baaa487b119da7fb21
thek4n <thek4n@yandex.ru>
2025-11-05 22:17:50 +0300

    move function to scripts

--- TARGETS.sh
+++ TARGETS.sh
@@ -25,5 +25,5 @@ gdb:.config/gdb
 sandbox:
 utils:.local/bin/myip .local/bin/genpass .local/bin/gensalt .local/bin/django-create-project .local/bin/mirror-site .local/bin/split-file .local/bin/extract .local/bin/destroy
 chromium:.local/bin/pchromium .local/share/applications/pchromium.desktop
-scripts:.local/bin/httpstatus .local/bin/bb .local/bin/emoji .local/bin/mksh .local/bin/nato .local/bin/pastas .local/bin/timer .local/bin/tryna .local/bin/trynafail
+scripts:.local/bin/httpstatus .local/bin/bb .local/bin/emoji .local/bin/mksh .local/bin/nato .local/bin/pastas .local/bin/timer .local/bin/tryna .local/bin/trynafail .local/bin/bak .local/bin/cleanup-directory .local/bin/cleanup-directory-log .local/bin/cleanup-downloads .local/bin/gobuild .local/bin/json .local/bin/mcd .local/bin/open .local/bin/py .local/bin/sha .local/bin/showtips .local/bin/weather
 all:%less %t %note %tmux %zsh %nvim %git"
