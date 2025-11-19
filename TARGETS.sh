e4a7358443ca60e1d0fa98bdafed2cda97bcb2b5
thek4n <thek4n@yandex.ru>
2025-11-19 22:51:30 +0300

    add script wake

--- TARGETS.sh
+++ TARGETS.sh
@@ -25,6 +25,6 @@ gdb:.config/gdb
 sandbox:
 utils:.local/bin/myip .local/bin/genpass .local/bin/gensalt .local/bin/django-create-project .local/bin/mirror-site .local/bin/split-file .local/bin/extract .local/bin/destroy
 chromium:.local/bin/pchromium .local/share/applications/pchromium.desktop
-scripts:.local/bin/httpstatus .local/bin/bb .local/bin/emoji .local/bin/mksh .local/bin/nato .local/bin/pastas .local/bin/timer .local/bin/tryna .local/bin/trynafail .local/bin/bak .local/bin/cleanup-directory .local/bin/cleanup-directory-log .local/bin/cleanup-downloads .local/bin/gobuild .local/bin/json .local/bin/open .local/bin/py .local/bin/sha .local/bin/showtips .local/bin/weather .local/bin/radio .local/bin/rmt .local/bin/scratch .local/bin/tunes %copypasta
+scripts:.local/bin/httpstatus .local/bin/bb .local/bin/emoji .local/bin/mksh .local/bin/nato .local/bin/pastas .local/bin/timer .local/bin/tryna .local/bin/trynafail .local/bin/bak .local/bin/cleanup-directory .local/bin/cleanup-directory-log .local/bin/cleanup-downloads .local/bin/gobuild .local/bin/json .local/bin/open .local/bin/py .local/bin/sha .local/bin/showtips .local/bin/weather .local/bin/radio .local/bin/rmt .local/bin/scratch .local/bin/tunes .local/bin/wake %copypasta
 copypasta:.local/bin/copy .local/bin/pasta
 all:%less %t %note %tmux %zsh %nvim %git"
